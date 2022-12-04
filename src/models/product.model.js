78fc1a39a363a3b7424bde8fb909a68d56e48140
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2022-12-04 18:41:58 -0300

    Requisito 02

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -2,7 +2,6 @@ const connection = require('./connection');
 
 const findAll = async () => {
   const [result] = await connection.execute('SELECT * FROM products');
-  console.log(result);
   return result;
 };
 
