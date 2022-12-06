040a208cf5a0014f75037f674983cf41592b79ca
Sylvio Nobrega Soares <sylvionobregasoares@gmail.com>
2022-12-06 20:12:20 -0300

    TDD - Listar sales e salesId, MSC - Controllers

--- src/controllers/sales.controller.js
+++ src/controllers/sales.controller.js
@@ -1,6 +1,16 @@
-const findAll = () => { };
+const { salesService } = require('../services');
 
-const findById = () => { };
+const findAll = async (_req, res) => {
+  const { message } = await salesService.findAll();
+  return res.status(200).json(message);
+};
+
+const findById = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await salesService.findById(id);
+  if (type) return res.status(404).json({ message });
+  return res.status(200).json(message);
+};
 
 module.exports = {
   findAll,
